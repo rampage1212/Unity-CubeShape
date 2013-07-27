b4dd779736579cc5bf951e606c6254e3c30c8451
rampage0131 <jake.docker0131@gmail.com>
2013-07-27 23:42:30 +0200

    Camera controls

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+    private const float cameraDistance = 10.0f;
+    private const float animationDuration = 1.0f;
+
+	void Start() {
+
+	}
+
+	void Update() {
+        if (iTween.Count(gameObject) == 0) {
+            Vector3 movement = Vector3.zero;
+            Vector3 rotation = Vector3.zero;
+
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                movement = transform.right + transform.forward;
+                rotation.y = -0.25f;
+            } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                movement = -transform.right + transform.forward;
+                rotation.y = 0.25f;
+            } else if (Input.GetKeyDown(KeyCode.UpArrow)) {                
+                movement = transform.up + transform.forward;
+                rotation.x = 0.25f;
+            } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                movement = -transform.up + transform.forward;
+                rotation.x = -0.25f;
+            }
+
+            if (!movement.Equals(Vector3.zero) || !rotation.Equals(Vector3.zero)) {
+                iTween.MoveTo(gameObject, transform.position + movement * cameraDistance, animationDuration);
+                iTween.RotateBy(gameObject, rotation, animationDuration);
+            }
+        }
+	}
+}
