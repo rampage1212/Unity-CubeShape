45161dda3ea1be4e09e2ae959145b93aaf28e024
rampage0131 <jake.docker0131@gmail.com>
2013-07-29 17:42:13 +0200

    Player's movement

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -3,34 +3,48 @@ using System.Collections;
 
 public class CameraControls : MonoBehaviour {
 
-    private const float cameraDistance = 10.0f;
+    private const float cameraDistance = 13.0f;
     private const float animationDuration = 1.0f;
 
 	void Start() {
 
 	}
 
+    void OnGUI() {
+        if (GameObject.Find("PlayerCube") == null) {
+            GUILayout.Label("Game Over");
+        }
+    }
+
 	void Update() {
         if (iTween.Count(gameObject) == 0) {
             Vector3 movement = Vector3.zero;
             Vector3 rotation = Vector3.zero;
 
-            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (Input.GetKeyDown(KeyCode.D)) {
                 movement = transform.right + transform.forward;
                 rotation.y = -0.25f;
-            } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            } else if (Input.GetKeyDown(KeyCode.A)) {
                 movement = -transform.right + transform.forward;
                 rotation.y = 0.25f;
-            } else if (Input.GetKeyDown(KeyCode.UpArrow)) {                
+            } else if (Input.GetKeyDown(KeyCode.W)) {                
                 movement = transform.up + transform.forward;
                 rotation.x = 0.25f;
-            } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            } else if (Input.GetKeyDown(KeyCode.S)) {
                 movement = -transform.up + transform.forward;
                 rotation.x = -0.25f;
+            } else if (Input.GetKeyDown(KeyCode.Q)) {
+                rotation.z = -0.25f;
+            } else if (Input.GetKeyDown(KeyCode.E)) {
+                rotation.z = 0.25f;
             }
 
-            if (!movement.Equals(Vector3.zero) || !rotation.Equals(Vector3.zero)) {
+            if (!movement.Equals(Vector3.zero)) {
+                Debug.Log(transform.position + movement * cameraDistance);
                 iTween.MoveTo(gameObject, transform.position + movement * cameraDistance, animationDuration);
+            }
+
+            if (!rotation.Equals(Vector3.zero)) {
                 iTween.RotateBy(gameObject, rotation, animationDuration);
             }
         }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -3,13 +3,53 @@ using System.Collections;
 
 public class PlayerControls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    const float animationDuration = 10.0f;
+    const int mapSize = 10;
+
+    public Vector3 movement;
+
+	void Start() {
+        movement = Vector3.zero;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update() {
+
+        if (Vector3.Distance(transform.position, Vector3.zero) > 10.0f) {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (iTween.Count(gameObject) == 0) {
+            // if not moving at the moment
+            Transform direction = Camera.main.transform;
+            movement = Vector3.zero;
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                movement = -direction.right;
+            } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                movement = direction.right;
+            } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                movement = direction.up;
+            } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                movement = -direction.up;
+            }
+
+            if (!movement.Equals(Vector3.zero)) {
+                int i;
+                for (i = 1; i <= mapSize; i++) {
+                    if (Physics.OverlapSphere(transform.position + movement * i, 0.1f).Length > 0) {
+                        i--;
+                        break;
+                    }
+                }
+
+                if (i > mapSize) {
+                    i = 20;
+                }
+
+                iTween.MoveTo(gameObject, iTween.Hash("position", transform.position + movement * i, 
+                    "speed", animationDuration, "easetype", iTween.EaseType.linear));
+            }
+        }
 	}
 }
